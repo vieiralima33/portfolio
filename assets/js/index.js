e020959a048608e374c3157196e1adec1390807f
Filipe Lima <vieiralima33@gmail.com>
2025-02-27 15:54:18 +0000

    books of bible wip

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,26 @@ function loadPage(page) {
         .then(response => response.text())
         .then(html => {
             document.getElementById('content').innerHTML = html;
+            loadPageScript(page); 
         })
         .catch(error => console.error('Error loading page:', error));
 }
+
+function loadPageScript(page) {
+    let scriptPath = '';
+    if (page.includes('projects.html')) {
+        scriptPath = './assets/js/projects.js';
+    }
+
+    if (scriptPath) {
+        const script = document.createElement('script');
+        script.src = scriptPath;
+        script.defer = true;
+        script.dataset.pagina = true;
+        document.body.appendChild(script);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     loadPage('./src/about.html');
 });
